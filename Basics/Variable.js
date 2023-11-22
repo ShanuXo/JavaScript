232949b96c64e9d12a122033154104992fd34d03
Shanu kumar <96777072+ShanuXo@users.noreply.github.com>
2023-11-22 21:20:48 +0000

    Variable : Let var const

---
+++ Basics/Variable.js
@@ -0,0 +1,17 @@
+const accountId=5481
+let accountEmail="shanu@gmail.com"
+var accountPassword="shanu04"
+accountCity="Bhagalpur"
+let accountState;
+
+//accountId=2 //Not allowed
+
+accountEmail="test@gmail.com"
+accountPassword="shanu12";
+accountCity="bangalore"
+
+/*
+Prefer not to use Var bacause of function scope and block scope issue
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
