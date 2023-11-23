9d904ee5e224fade2019f518c7f9cf6488796bb9
Shanu kumar <96777072+ShanuXo@users.noreply.github.com>
2023-11-23 19:32:05 +0000

    Datatype

---
+++ Basics/Datatype.js
@@ -0,0 +1,24 @@
+"use strict" // treat all JS code as newer version
+// alert(3+3); // We are using Nodejs not browser
+//www.tc39.es
+let name="Shanu"
+let age=18
+let isLoggedIn=true
+console.table([name,age,isLoggedIn])
+
+/* Primitive Datatype
+Number = 2 to power 53
+bigint
+string =""
+boolean = true/false
+null= standalone value(null is basically representation of empty value)
+undefined
+symbol(to find uniqueness)
+*/
+
+/* Object
+
+*/
+console.log(typeof "Shanu");
+console.log(typeof null);
+console.log(typeof undefined);
