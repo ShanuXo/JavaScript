c814d62b91e2c863a6d3295ff8e7b04fbb853c37
Shanu kumar <96777072+ShanuXo@users.noreply.github.com>
2023-11-26 14:10:10 +0000

    Undefined Vs Not Defined

---
+++ Advance/UndefinedVsNotDefined.js
@@ -0,0 +1,21 @@
+// Javscript is a loosley Typed language
+//Example
+let z=10
+console.log(z)
+z="Shanu"
+console.log(z)
+
+
+//Undefined : works as a "Placeholder" for any variables
+console.log(a) // ReferenceError: Cannot access 'a' before initialization
+let a=10 // Since we are not printing the value of "a",JS will allocates some memory to a and with the value Undefined
+ let b;
+ if(b===undefined)
+ {
+    console.log("a is  Undefined")
+ }else{
+    console.log("a is not Undefined")
+ }
+
+// Not-Defined
+console.log(x) //OP : ReferenceError: x is not defined
