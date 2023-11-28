ac244d97cdc3e0956c02af7a732171878062f535
Shanu kumar <96777072+ShanuXo@users.noreply.github.com>
2023-11-28 06:42:48 +0000

    Array in JS

---
+++ Basics/Array.js
@@ -0,0 +1,33 @@
+//In JavaScript array can contains multiple datatype in a single array
+let myArr=[1,2,3,4,5]
+console.log(myArr);
+myArr.push(6);
+myArr.push(7);//insert at last
+myArr.unshift(9);//Insert at first step
+myArr.shift();
+myArr.pop()
+console.log(myArr);
+
+//Shallow Copy:
+/*
+A shallow copy of an object is a copy whose properties share the same references (point to the same underlying values) as those of the source object from which the copy was made. As a result, when you change either the source or the copy, you may also cause the other object to change too. That behavior contrasts with the behavior of a deep copy, in which the source and copy are completely independent.
+Example code : 
+const ingredientsList = ["noodles", { list: ["eggs", "flour", "water"] }];
+
+const ingredientsListCopy = Array.from(ingredientsList);
+console.log(ingredientsListCopy);
+// ["noodles",{"list":["eggs","flour","water"]}]
+
+*/
+
+/*
+Deep Copy :
+A deep copy of an object is a copy whose properties do not share the same references (point to the same underlying values) as those of the source object from which the copy was made. As a result, when you change either the source or the copy, you can be assured you're not causing the other object to change too. That behavior contrasts with the behavior of a shallow copy, in which changes to nested properties in the source or the copy may cause the other object to change too.
+
+Example Code : 
+
+const ingredientsList = ["noodles", { list: ["eggs", "flour", "water"] }];
+const ingredientsListDeepCopy = JSON.parse(JSON.stringify(ingredientsList));
+
+*/
+
