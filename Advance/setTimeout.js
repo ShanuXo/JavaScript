2003987af299f8cb409d91057640a29b844642e4
Shanu kumar <96777072+ShanuXo@users.noreply.github.com>
2023-12-09 22:19:25 +0000

    SetTimeout in JS

---
+++ Advance/setTimeout.js
@@ -0,0 +1,11 @@
+// *******************SETTIMEOUT***********************
+function x()
+{
+    var i="Shanu"
+    setTimeout(function()
+    {
+        console.log(i)
+    },3000);
+    console.log(`Hey ${i} welcome to Namaste Javascript`);
+}
+x();
