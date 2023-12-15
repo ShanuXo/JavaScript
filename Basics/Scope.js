13d6a358ecd0fbb5df245e2ab96d239d8700d60e
Shanu kumar <96777072+ShanuXo@users.noreply.github.com>
2023-12-15 07:59:49 +0000

    Scope

---
+++ Basics/Scope.js
@@ -0,0 +1,33 @@
+let a=10;
+var b=20;
+const c=30;
+console.log(a);
+console.log(b);
+console.log(c);
+
+
+var z=50;
+let x=10;
+if(true)
+{
+    let x=100;
+    const y=200;
+    z=300;
+    console.log(`Value of X : ${x}`)
+}
+console.log(x)
+//console.log(y)
+console.log(z)
+
+
+var myArry=[1,2,3,4,5]
+function sum(myArray)
+{
+    let sum=0;
+    for(let i=0;i<myArry.length;i++)
+    {
+        sum+=myArray[i];
+    }
+    return sum;
+}
+console.log(sum(myArry));
