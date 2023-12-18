ee7a9aa13566a23eb0c9e6205781639eaa8910e2
Shanu kumar <96777072+ShanuXo@users.noreply.github.com>
2023-12-18 08:01:00 +0000

    Switch

---
+++ Basics/Switch.js
@@ -0,0 +1,31 @@
+/** Syntax of Switch: 
+ switch(key)
+ {
+    case value:
+
+    break;
+
+    default:
+        break;
+ }
+ */
+
+ const month=4;
+ switch(month)
+ {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("Febuary");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    default:
+        console.log("default case match")
+        break;    
+ }
