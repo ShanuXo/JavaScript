efd91d1ed6b5c5caa1ddeebb62ced4853fd85565
Shanu kumar <96777072+ShanuXo@users.noreply.github.com>
2023-12-18 09:31:33 +0000

    Truthy Falsy in JS

--- Basics/TruthyFalsy.js
+++ Basics/TruthyFalsy.js
@@ -15,3 +15,36 @@ if("ai@gmail.com"===userEmail[1])
 }else{
     console.log("Don't have user Details");
 }
+
+/*
+Falsy Values : false,0,-0,BigInt 0n,"",null,undefined,NaN
+
+Truthy Values: "0",'false'," ",[],{},function(){}
+*/
+
+const emptyObject={}
+
+if(Object.keys(emptyObject).length===0)
+{
+    console.log("Object is empty");
+}
+
+//Nullish Coalescing Operator(??)
+let value;
+value=5 ?? 10;
+let value1=null??10;
+let value2=undefined??15;
+let value3=null??10??15;
+console.log(value);
+console.log(value1);
+console.log(value2);
+console.log(value3);
+
+
+//Terniary Operator
+/*
+condition ? true : false;
+*/
+
+const iceTea=100;
+iceTea>=80?console.log("YES"):console.log("NO");
